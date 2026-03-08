a169a891c9ddf2092e901ac68cf70e6743b98a21
James Bakis <s3783333@student.rmit.edu.au>
2026-03-08 21:23:56 +1100

    Background config and labels

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-from tkinter import *
+import tkinter as tk
 
 def main():
     print("hello")
-    root = Tk()
+    root = tk.Tk()
+    root.title("Collect Crystals")
+    root.configure(background="red")
+    root.minsize(720, 480)
+    root.maxsize(720, 480)
+    root.geometry("720x480+0+0")
+    
+    label1 = tk.Label(root, text="Collect crystals!")
+    label2 = tk.Label(root, text="Avoid enemies!")
+    label1.pack()
+    label2.pack()
+    
     root.mainloop()
+    
 
 if __name__ == "__main__":
     main()
